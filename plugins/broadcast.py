174c5231a92773984a38bb73423f3362a6dac2ac
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-08-16 09:35:18 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -5,7 +5,7 @@ import asyncio
 import datetime
 import aiofiles.os
 import traceback
-from config import Config
+from plugins.config import Config
 from helpers.database.access_db import db
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
