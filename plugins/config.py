66a3c69388d9c1f967b440da81aca9dfec073f20
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-08-16 09:47:29 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -7,7 +7,7 @@ class Config(object):
 
     # The Telegram API things
     # Get these values from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
 
     # Array to store users who are authorized to use the bot
