fb0718e2ff0d6cb109113b90bb4f6a10f9cafe45
Jack <125857863+Singhsawan@users.noreply.github.com>
2023-08-22 20:07:18 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -13,7 +13,7 @@ import time
 if bool(os.environ.get("WEBHOOK", False)):
     from plugins.config import Config
 else:
-    from plugins.config import Config
+    from sample_config import Config
 
 from datetime import datetime
 from hachoir.parser import createParser
