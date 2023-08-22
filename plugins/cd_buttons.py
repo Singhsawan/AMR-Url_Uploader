031880a60921345e8a3f9db0f641748165b095e1
Jack <125857863+Singhsawan@users.noreply.github.com>
2023-08-22 20:06:21 +0530

    Update cd_buttons.py

--- plugins/cd_buttons.py
+++ plugins/cd_buttons.py
@@ -14,7 +14,7 @@ import time
 if bool(os.environ.get("WEBHOOK", False)):
     from plugins.config import Config
 else:
-    from plugins.config import Config
+    from sample_config import Config
 
 from plugins.youtube_dl_button import youtube_dl_call_back
 from plugins.dl_button import ddl_call_back
